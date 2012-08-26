c8ccd42fb6cdeec5430f61dfa454ab1166bc16a0
spencer <spencer@spencer-desktop.(none)>
2012-08-26 00:29:53 -0400

    INSTALL: fixed some stuff made it work, untested fully.

--- install-windows.bat
+++ install-windows.bat
@@ -2,12 +2,12 @@
 cls
 
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-echo
+echo.
 echo  TWRP 2.2.2.0 Automated Install    
-echo	   Iconia Tab A100             
-echo		by
-echo	     Godmachine81               
-echo
+echo        Iconia Tab A100             
+echo              by
+echo         Godmachine81               
+echo.
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 
@@ -19,33 +19,34 @@ echo if you would like to continue with
 echo installation on A100 then press Y
 echo otherwise press N to abort
 
-IF '%var%' == Y goto start
-IF '%var%' == y goto start
-IF '%var%' == N goto no
-IF '%var%' == n goto no
+set /p var= please make a selction...
+
+
+IF '%var%' == 'Y' goto start
+IF '%var%' == 'y' goto start
+IF '%var%' == 'N' goto no
+IF '%var%' == 'n' goto no
 
 :no
 cls
+color 40
 echo You chose to abort installation
 echo Exiting...
 pause 
 exit
 
-:yes
-
+:start
 cls
 echo Installation started, installing TWRP to your A100
-
 pause
 echo Preparing adb... 
 
 adb kill-server
-adb root
 
 echo Checking if stock recover script exists and moving if so
 adb shell su -c "mount -o remount, rw /system/; mv /etc/install-recovery.sh /etc/install-recovery.offsh;"
 
-echo
+echo.
 
 echo Rebooting to Bootloader
 adb reboot bootloader
@@ -55,17 +56,14 @@ echo
 echo Preparing to Flash
 fastboot flash recovery recovery.img
 echo Recovery flashed, rebooting
+echo hold the volume rocker closest to rotation lock to reboot recovery
 fastboot reboot
-echo Waiting for adb connection...
-adb wait-for-device
-echo Device Ready, rebooting into recovery
-adb reboot recovery
-cls
-echo Your device should now boot into TWRP
+echo Your device should now boot into System,
 echo please give your device a minute to 
-echo reboot and boot to recovery.
-echo
+echo reboot and boot to recovery to verify it flashed
+echo.
+color A2
+echo install complete!
 echo Thanks for using the A100 Automated
-echo Installation Script - Godmachine81
-
+echo Installation Script - Xbooow59
 pause > null
